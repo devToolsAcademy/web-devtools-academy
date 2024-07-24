4243970dadc380f398f3fee80d649bb3452ef91f
AJimenezHolguin <anjimhol23@gmail.com>
2024-07-23 19:56:49 -0500

    creation of a dynamic button component that allows receiving props by parameters.

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,7 +1,35 @@
+import { Button } from '@nextui-org/react';
+import { Color, Variant} from './types';
 import React from 'react'
+import Styles from './Button.module.css';
+import   { Props }  from './interface';
 
-export const Button = () => {
+
+const colorClass: {[key in Color]:string} = {
+  white:Styles.ButtonWhite,
+  blue: Styles.ButtonBlue,
+  green: Styles.ButtonGreen,
+  sinFondo: Styles.ButtonSinFondo
+}
+
+export const Button1:React.FC<Props>= ({color,width,variant,label,onClick,startContent,endContent,isLoading }) => {
+  const colorClasses = colorClass[color]
+
   return (
-    <div>Button</div>
+    <div>
+      <Button 
+      className={colorClasses} 
+      style={{width: `${width}px`}}
+      variant = {variant}
+      onClick={onClick}
+      startContent={startContent}
+      endContent={endContent}
+      isLoading={isLoading}
+      >
+        <span>{label}</span>
+      </Button>
+    </div>
   )
 }
+
+
---
+++ src/shared/ui/Button/types.ts
@@ -0,0 +1,3 @@
+export type Color = 'white' | 'blue' | 'green'| 'sinFondo';
+export type Variant = 'ghost' | 'solid' | 'bordered' | 'light' | 'shadow';
+
