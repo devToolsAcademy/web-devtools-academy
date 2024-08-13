a35670ed096dc6857bd1fe2036632c616e5daf70
miguelcastillo927 <al.m.castillo.927@gmail.com>
2024-08-13 09:56:30 -0500

    creation inputcomponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import InputComponent from '../shared/ui/Input/Input';
-
-import IconMail from '../shared/ui/icon/mail';
-import IconUser from '../shared/ui/icon/user';
-// import '../shared/ui/styles/InputComponent.css';
-
 export default function Home() {
-  return (
-    <main>
-      Devtools
-      <InputComponent
-        label="Contraseña"
-        type="text"
-        variant="underlined"
-        placeholder="Ingrese su contraseña"
-        // endContent={<IconUser />}
-      />
-      <InputComponent
-        label="hola"
-        required={true}
-        startContent={<IconUser className="text-xl text-default-400 pointer-events-none flex-shrink-0" />}
-      />
-      <InputComponent label="" endContent={<IconMail />} />
-    </main>
-  );
+  return <main>Devtools</main>;
 }
--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -2,33 +2,30 @@
 
 import React from 'react';
 import { Input } from '@nextui-org/react';
-import IconEye from '../icon/eye';
-import IconEyeInvisible from '../icon/eyeSlash';
-import IconMail from '../icon/mail';
+import IconEye from '../icon/input/eye';
+import IconEyeInvisible from '../icon/input/eyeSlash';
 
 interface InputComponentProps {
   label: string;
-  type?: "text" | "email" | "password" | string;
+  type?: 'text' | 'email' | 'password' | string;
   icon?: React.ReactNode;
   required?: boolean;
   placeholder?: string;
-  text?: string;
-  error?: boolean;
-  
+  invalid?: boolean;
+  errorMessage?: string;
   endContent?: React.ReactNode;
   startContent?: React.ReactNode;
   variant?: 'underlined' | 'flat' | 'faded' | 'bordered';
 }
 
 const InputComponent: React.FC<InputComponentProps> = ({
-  //show an information text
   label,
   type = 'text',
   icon,
   required = false,
   placeholder = '',
-  text = '',
-  error = false,
+  invalid = false,
+  errorMessage = '',
   endContent,
   startContent,
   variant = 'underlined'
@@ -40,10 +37,9 @@ const InputComponent: React.FC<InputComponentProps> = ({
       return endContent;
     } else if (type === 'password') {
       return (
-
         <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
           {isVisible ? (
-            < IconEye className="text-2xl text-default-400 pointer-events-none" />
+            <IconEye className="text-2xl text-default-400 pointer-events-none" />
           ) : (
             <IconEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
           )}
@@ -52,24 +48,21 @@ const InputComponent: React.FC<InputComponentProps> = ({
     }
     return null;
   };
-  console.log("hola mundo")
+
   return (
     <div className="input-group">
       {icon && <span className="icon">{icon}</span>}
       <Input
-        isRequired ={required}
+        isRequired={required}
         variant={variant}
         type={type === 'password' ? (isVisible ? 'text' : 'password') : type}
         label={label}
         placeholder={placeholder}
-        
         endContent={renderEndContent()}
         startContent={startContent}
-        
-        className={`input ${variant} ${error ? 'error' : ''}`}
+        errorMessage={errorMessage}
+        isInvalid={invalid}
       />
-      {error && <label className="error-label">{label}*</label>}
-      {!error && text && <div className="input-helper-text">{text}</div>}
     </div>
   );
 };
